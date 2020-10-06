5fa2b8e3a3e0266bff0778a2092b03e05272cec1
massgh <massgh@users.noreply.github.com>
2020-10-06 19:53:04 -0400

    ++++

--- app/__main__.py
+++ app/__main__.py
@@ -4,7 +4,14 @@ import os
 path = os.path.dirname(__file__)
 app_path = os.path.join(path,'index.py')
 
+try:
+    interpreter = sys.executable
+except:
+    interpreter = "python"
+
 if len(sys.argv) > 1:
-    os.system("python {} {}".format(app_path,sys.argv[1]))
+    print("Running Pivotpy-Dash App using '{}'".format(interpreter))
+    os.system("{} {} {}".format(interpreter, app_path,sys.argv[1]))
 else:
-    os.system("python {}".format(app_path))
+    print("Running Pivotpy-Dash App using '{}'".format(interpreter))
+    os.system("{} {}".format(interpreter, app_path))
--- app/pages/locpot.py
+++ app/pages/locpot.py
@@ -2,29 +2,39 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from app import app
+
+
+import pivotpy as pp 
+import matplotlib.pyplot as plt
+pp.quick_rgb_lines("E:/Research/graphene_example/ISPIN_1/bands/vasprun.xml",elim=[-9,9])
+plot = pp.plt_to_html(dpi=1200,dash_html=True)
 layout = html.Div(
     [
         dbc.Button(
-            "Open collapse",
+            "Open Matplotlib",
             id="collapse-button",
             className="btn-simple",
             color="primary",
         ),
-        dbc.Collapse(
-            dbc.Card(dbc.CardBody("This content is hidden in the collapse")),
+        dbc.Collapse([html.H3("Matplotlib Figure",className='card-title'),plot],
             id="collapse",
         ),
-        html.Div(html.H6('After Collapse'))
+        html.Div(html.H6('After Collapse')),
+        
     ]
 )
 
 
 @app.callback(
-    Output("collapse", "is_open"),
+    [Output("collapse", "is_open"),Output("collapse-button","children")],
     [Input("collapse-button", "n_clicks")],
-    [State("collapse", "is_open")],
+    [State("collapse", "is_open"),State("collapse-button","children")],
 )
-def toggle_collapse(n, is_open):
+def toggle_collapse(n, is_open,child):
+    if "Open" in child:
+        label = "Close Matplotlib"
+    else:
+        label = "Open Matplotlib"
     if n:
-        return not is_open
-    return is_open
+        return not is_open,label
+    return is_open,label
